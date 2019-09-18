9094012d9835cb2ce7e7409faa48f016a88597b1
ericknegreter <erickxon-roy@hotmail.com>
2019-09-18 13:08:28 -0500

    agregue un except para controlar las posibles fallas no especificadas y en ultrasonic, modifique parte del codigo agregando un while para mejorar el performance

--- extraccion_distancia/ultrasonic_distance.py
+++ extraccion_distancia/ultrasonic_distance.py
@@ -6,21 +6,32 @@ import os
 import subprocess, datetime
 
 #GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
 
 #set GPIO Pins
 GPIO_TRIGGER = 5
 GPIO_ECHO = 6
  
 #set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-GPIO.setup(13, GPIO.OUT)
+#GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+#GPIO.setup(GPIO_ECHO, GPIO.IN)
+#GPIO.setup(13, GPIO.OUT)
 
 #test host
 hosts = ('google.com', 'kernel.org', 'yahoo.com')
 localhost = ('10.0.5.246')
+
+def setup():
+    #GPIO Mode (BOARD / BCM)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    #set GPIO direction (IN / OUT)
+    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+    GPIO.setup(GPIO_ECHO, GPIO.IN)
+    GPIO.setup(13, GPIO.OUT)
+
 def ping(host):
     ret = subprocess.call(['ping', '-c', '3', '-W', '5', host],
             stdout=open('/dev/null', 'w'),
@@ -70,51 +81,60 @@ def distance():
     return distance
  
 if __name__ == '__main__':
-    try:
-        estado = 0
-        while True:
-            dist = distance()
-            print("Measured Distance = %.1f cm" % dist)
-            #time.sleep(1)
-            if(dist >= 35 and dist <= 38):
-                if(estado != 0):
-                    while True:
-                        if(net_is_up() == 0):
-                            #Connection to database LMV and insert on registro table new field with mysql
-                            #registro
-                            mydb = mysql.connector.connect(host="10.0.5.246", user="LMV_ADMIN", passwd="LABORATORIOT4", database="LMV")
-                            mycursor = mydb.cursor()
-                            sql = "UPDATE e_extraccion SET estado = 0 WHERE dispositivo='transfer'"
-                            mycursor.execute(sql)
-                            mydb.commit()
-                            print(mycursor.rowcount, "record affected.")
-                            time.sleep(1)
-                            #END of mysql
-                            estado = 0
-                            break 
-                    #Led start
-                    GPIO.output(13, False)
-            else:
-                if(estado != 1):
-                    while True:
-                        if(net_is_up() == 0):
-                            #Connection to database LMV and insert on registro table new field with sql
-                            #registro
-                            mydb = mysql.connector.connect(host="10.0.5.246", user="LMV_ADMIN", passwd="LABORATORIOT4", database="LMV")
-                            mycursor = mydb.cursor()
-                            sql = "UPDATE e_extraccion SET estado = 1 WHERE dispositivo='transfer'"
-                            mycursor.execute(sql)
-                            mydb.commit()
-                            print(mycursor.rowcount, "record affected.")
-                            time.sleep(1)
-                            #END of mysql
-                            estado = 1
-                            break
-                    #Start led
-                    GPIO.output(13, True)
-            time.sleep(5)
+    while True:
+        try:
+            estado = 0
+            setup()
+            while True:
+                dist = distance()
+                print("Measured Distance = %.1f cm" % dist)
+                #time.sleep(1)
+                if(dist >= 35 and dist <= 38):
+                    if(estado != 0):
+                        while True:
+                            if(net_is_up() == 0):
+                                #Connection to database LMV and insert on registro table new field with mysql
+                                #registro
+                                mydb = mysql.connector.connect(host="10.0.5.246", user="LMV_ADMIN", passwd="LABORATORIOT4", database="LMV")
+                                mycursor = mydb.cursor()
+                                sql = "UPDATE e_extraccion SET estado = 0 WHERE dispositivo='transfer'"
+                                mycursor.execute(sql)
+                                mydb.commit()
+                                print(mycursor.rowcount, "record affected.")
+                                time.sleep(1)
+                                #END of mysql
+                                estado = 0
+                                break 
+                        #Led start
+                        GPIO.output(13, False)
+                else:
+                    if(estado != 1):
+                        while True:
+                            if(net_is_up() == 0):
+                                #Connection to database LMV and insert on registro table new field with sql
+                                #registro
+                                mydb = mysql.connector.connect(host="10.0.5.246", user="LMV_ADMIN", passwd="LABORATORIOT4", database="LMV")
+                                mycursor = mydb.cursor()
+                                sql = "UPDATE e_extraccion SET estado = 1 WHERE dispositivo='transfer'"
+                                mycursor.execute(sql)
+                                mydb.commit()
+                                print(mycursor.rowcount, "record affected.")
+                                time.sleep(1)
+                                #END of mysql
+                                estado = 1
+                                break
+                        #Start led
+                        GPIO.output(13, True)
+                time.sleep(5)
 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        #GPIO.cleanup()
+            # Reset by pressing CTRL + C
+        except KeyboardInterrupt:
+            print("Measurement stopped by User")
+        except ValueError;
+            print("Measurement stopped by error")
+        except OSError as err:
+            print("OS error: {0}".format(err))
+        except:
+            print("no controlado")
+        finally:
+            GPIO.cleanup()
